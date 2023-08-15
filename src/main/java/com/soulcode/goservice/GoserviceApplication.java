9f2d7e261b7c0d21543e15e7ff090771a95847ab
victoricoma-soulcode <victor.icoma@soulcode.com>
2023-08-15 09:11:53 -0300

    Setup inicial

---
+++ src/main/java/com/soulcode/goservice/GoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.soulcode.goservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soulcode/goservice/GoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulcode.goservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
