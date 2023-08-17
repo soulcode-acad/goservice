cdbca5e5ebb5a94d7fd5c62c0f86bc55d98eee49
Nicoli <nicoli.rodriguesslopes@gmail.com>
2023-08-17 15:13:35 -0300

    Inicialização

---
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/GoserviceappApplication.java
@@ -0,0 +1,13 @@
+package com.soulcode.goserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoserviceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoserviceappApplication.class, args);
+	}
+
+}
