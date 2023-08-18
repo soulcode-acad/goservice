aef4a968341b0c6043f8812fef1f5e216a9c784e
gabrieljdantas <gabrieljdantas26@gmail.com>
2023-08-18 10:49:32 -0300

    Adicionado Controller de html

---
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
@@ -0,0 +1,10 @@
+package com.soulcode.goserviceapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/")
+public class HomeController {
+
+}
