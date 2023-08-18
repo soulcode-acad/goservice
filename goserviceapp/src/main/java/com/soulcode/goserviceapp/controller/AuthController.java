b75ad73e4d4a2d624f968956857c51b108a6db08
Lucas Figueiredo <91294318+DomLucaz@users.noreply.github.com>
2023-08-18 18:02:14 -0300

    Refactor: criação dos componentes do fragments, construção do layouthtml utilizando Thymeleaf

---
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/controller/AuthController.java
@@ -0,0 +1,16 @@
+package com.soulcode.goserviceapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping (value = "/auth")
+public class AuthController {
+
+    @GetMapping(value = "/login")
+    public String login(){return "login";}
+
+    @GetMapping(value = "/cadastroCliente")
+    public String cadastro(){return "cadastroCliente";}
+}
