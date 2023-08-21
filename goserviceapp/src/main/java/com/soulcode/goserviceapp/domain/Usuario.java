8a40b292316cfaac9ec466ddd6fffd4c48f73170
luc4sleite <lucahleite@gmail.com>
2023-08-21 13:38:46 -0300

    feat: administrador, usuario, cliente, prestador, servico e agendamento

--- goserviceapp/src/main/java/com/soulcode/goserviceapp/domain/Usuario.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/domain/Usuario.java
@@ -127,13 +127,21 @@ public class Usuario implements UserDetails {
     public boolean isEnabled() {
         return false;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass()!= o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id) &&
+                Objects.equals(nome, usuario.nome) &&
+                Objects.equals(email, usuario.email) &&
+                Objects.equals(senha, usuario.senha) &&
+                perfil == usuario.perfil;
+    }
     @Override
     public int hashCode() {
         return Objects.hash(id, nome, email, senha, perfil);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
 }
