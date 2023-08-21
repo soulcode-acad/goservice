32867437da3b0a423d875ec47d37bbdcb1070dcc
drygalapedro <drygalapedro@gmail.com>
2023-08-21 13:43:13 -0300

    Inserção de logs na classe controller e alteração de parâmetros de conexão com o banco de dados.

--- src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
+++ src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
@@ -1,15 +1,18 @@
 package com.soulcode.goserviceapp.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Log4j2
 @Controller
 @RequestMapping(value={"/", "/home"})
 public class HomeController {
 
     @GetMapping
     public String home(){
+        log.info("Iniciando aplicação na página home");
         return "home";
     }
 }
