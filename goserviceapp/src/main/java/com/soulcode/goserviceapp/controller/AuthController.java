c453eca4d604b7b17e989e294a4de98d5c24f652
gabrieljdantas <gabrieljdantas26@gmail.com>
2023-08-22 17:40:54 -0300

    Cadastro de novos clientes na pagina de cadastro

--- goserviceapp/src/main/java/com/soulcode/goserviceapp/controller/AuthController.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/controller/AuthController.java
@@ -1,14 +1,21 @@
 package com.soulcode.goserviceapp.controller;
 
+import com.soulcode.goserviceapp.domain.Cliente;
 import com.soulcode.goserviceapp.domain.Usuario;
+import com.soulcode.goserviceapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping(value = "/auth")
 public class AuthController {
+
+    @Autowired
+    private UserService usuarioService;
 
     @GetMapping(value = "/login")
     public String login(){
@@ -25,6 +32,16 @@ public class AuthController {
         return "alterarSenha";
 
     }
+    @PostMapping(value = "/signup")
+    public String cadastrarCliente(Cliente cliente, RedirectAttributes attributes){
+        try {
+            usuarioService.createUser(cliente);
+            attributes.addFlashAttribute("successMessage", "Cadastro realizado com sucesso");
+            return "redirect:/auth/login";
+        } catch (Exception ex){
+            attributes.addFlashAttribute("errorMessage", "Erro ao cadatrar novo cliente");
+            return "redirect:/auth/cadastro";
+        }
 
-
+    }
 }
