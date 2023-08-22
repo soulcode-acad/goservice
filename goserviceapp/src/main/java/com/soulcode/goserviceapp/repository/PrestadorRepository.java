ce9e753e9af0f36d13bb45067c1398e117ec2925
NgrCristian <cristian.barreto.ne@gmail.com>
2023-08-22 17:42:12 -0300

    Aplicações e Validações

--- goserviceapp/src/main/java/com/soulcode/goserviceapp/repository/PrestadorRepository.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/repository/PrestadorRepository.java
@@ -4,6 +4,11 @@ import com.soulcode.goserviceapp.domain.Prestador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PrestadorRepository extends JpaRepository<Prestador, Long> {
+
+    Optional<Prestador> findByEmail(String Email);
+
 }
