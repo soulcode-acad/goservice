6023901a35085c43cc4bf975d6437906e1cde312
gabrieljdantas <gabrieljdantas26@gmail.com>
2023-08-22 14:56:59 -0300

    Criado pagina de dados dos clientes

--- goserviceapp/src/main/java/com/soulcode/goserviceapp/config/SecurityConfig.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .requestMatchers("/**")
