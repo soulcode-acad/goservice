b206d7e57ca34b58a6c3253337ea8e56a46276ec
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-08-22 14:53:38 -0300

    Construção e mapeamento da página dados do Cliente

---
+++ src/main/java/com/soulcode/goserviceapp/controller/ClienteController.java
@@ -0,0 +1,15 @@
+package com.soulcode.goserviceapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/cliente")
+public class ClienteController {
+
+    @GetMapping(value = "/dados")
+    public String dados(){
+        return "dadosCliente";
+    }
+}
