ccaf19b5afca23a4c97b0dce036480276f46beb2
gabrieljdantas <gabrieljdantas26@gmail.com>
2023-08-23 15:19:17 -0300

    Criação do servico de servicos e atenticacao

--- goserviceapp/src/main/java/com/soulcode/goserviceapp/repository/UsuarioRepository.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/repository/UsuarioRepository.java
@@ -2,9 +2,16 @@ package com.soulcode.goserviceapp.repository;
 
 import com.soulcode.goserviceapp.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    Optional<Usuario> findByEmail(String email);
+
+    @Query(value = "UPDATE usuario AS u SET u.senha = ? WHERE u.email = ? ", nativeQuery = true)
+    void updatePasswordByEmail(String senha, String email);
 }
--- goserviceapp/src/main/java/com/soulcode/goserviceapp/service/ServicoService.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/service/ServicoService.java
@@ -1,4 +1,45 @@
 package com.soulcode.goserviceapp.service;
 
+import com.soulcode.goserviceapp.domain.Servico;
+import com.soulcode.goserviceapp.repository.ServicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
 public class ServicoService {
+
+    @Autowired
+    private ServicoRepository servicoRepository;
+
+    public List<Servico> findAll(){
+        return servicoRepository.findAll();
+    }
+
+    public Servico createServico(Servico servico){
+        servico.setId(null);
+        return servicoRepository.save(servico);
+    }
+
+    public void removeServicoById(Long id){
+        servicoRepository.deleteById(id);
+    }
+
+    public Servico findById(Long id){
+        Optional<Servico> servico = servicoRepository.findById(id);
+        if (servico.isPresent()){
+            return servico.get();
+        }else {
+            throw new RuntimeException("Serviço não encontrado");
+        }
+    }
+
+    public Servico update(Servico servico){
+        Servico updateServico = this.findById(servico.getId());
+        updateServico.setNome(servico.getNome());
+        updateServico.setDescricao(servico.getDescricao());
+        updateServico.setCategoria((servico.getCategoria()));
+        return servicoRepository.save(updateServico);
+    }
+
 }
