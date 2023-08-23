19de341a4e12a76958775f79a77aa0897196f4ae
victoricoma-soulcode <victor.icoma@soulcode.com>
2023-08-23 11:51:56 -0300

    Criação dos serviços básicos de Cliente e Prestador.

---
+++ src/main/java/com/soulcode/goserviceapp/service/ClienteService.java
@@ -0,0 +1,33 @@
+package com.soulcode.goserviceapp.service;
+
+import com.soulcode.goserviceapp.domain.Cliente;
+import com.soulcode.goserviceapp.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente findById(Long id){
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        if(cliente.isPresent()){
+            return cliente.get();
+        } else {
+            throw  new RuntimeException("Cliente não encontrado");
+        }
+    }
+    public Cliente update(Cliente cliente) {
+        Cliente updatedCliente = this.findById(cliente.getId());
+        updatedCliente.setNome(cliente.getNome());
+        updatedCliente.setEmail(cliente.getEmail());
+        updatedCliente.setTelefone(cliente.getTelefone());
+        updatedCliente.setCpf(cliente.getCpf());
+        updatedCliente.setDataNascimento(cliente.getDataNascimento());
+        return clienteRepository.save(updatedCliente);
+    }
+}
