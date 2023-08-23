14c4c0d2d23ab4c1c3ea378851e2c4630d840aa4
Lucas Figueiredo <91294318+DomLucaz@users.noreply.github.com>
2023-08-23 17:27:27 -0300

    Não lembro o que alteramos, mas agora tá autenticado e senha criptografada

--- goserviceapp/src/main/java/com/soulcode/goserviceapp/service/ClienteService.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/service/ClienteService.java
@@ -1,4 +1,34 @@
 package com.soulcode.goserviceapp.service;
 
+import com.soulcode.goserviceapp.domain.Cliente;
+import com.soulcode.goserviceapp.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente findById(Long id){
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        if (cliente.isPresent()){
+            return cliente.get();
+        }else {
+            throw new RuntimeException("Cliente não encontrado");
+        }
+    }
+
+    public Cliente update(Cliente cliente){
+        Cliente updateCliente = this.findById(cliente.getId());
+        updateCliente.setNome(cliente.getNome());
+        updateCliente.setEmail(cliente.getEmail());
+        updateCliente.setCpf(cliente.getCpf());
+        updateCliente.setTelefone(cliente.getTelefone());
+        updateCliente.setDataNascimento(cliente.getDataNascimento());
+        return clienteRepository.save(updateCliente);
+    }
 }
--- goserviceapp/src/main/java/com/soulcode/goserviceapp/service/PrestadorService.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/service/PrestadorService.java
@@ -1,4 +1,35 @@
 package com.soulcode.goserviceapp.service;
 
+import com.soulcode.goserviceapp.domain.Prestador;
+import com.soulcode.goserviceapp.repository.PrestadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class PrestadorService {
+
+    @Autowired
+    private PrestadorRepository prestadorRepository;
+
+    public Prestador findById(Long id) {
+        Optional<Prestador> prestador = prestadorRepository.findById(id);
+        if (prestador.isPresent()) {
+            return prestador.get();
+        } else {
+            throw new RuntimeException("Prestador não encontrado");
+        }
+    }
+
+    public Prestador update(Prestador prestador){
+        Prestador updatePrestador = this.findById(prestador.getId());
+        updatePrestador.setNome(prestador.getNome());
+        updatePrestador.setEmail(prestador.getEmail());
+        updatePrestador.setDescricao(prestador.getDescricao());
+        updatePrestador.setTaxaPorHora(prestador.getTaxaPorHora());
+        return prestadorRepository.save(updatePrestador);
+    }
 }
+
+
