82b38e94e52f7832f4b49f78da21af1d6f94fcca
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-08-23 15:16:18 -0300

    Construção do serviço AuthService.

--- src/main/java/com/soulcode/goserviceapp/repository/UsuarioRepository.java
+++ src/main/java/com/soulcode/goserviceapp/repository/UsuarioRepository.java
@@ -2,9 +2,18 @@ package com.soulcode.goserviceapp.repository;
 
 import com.soulcode.goserviceapp.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+    Optional<Usuario> findByEmail(String email);
+
+    @Modifying
+    @Query(value = "UPDATE usuarios u SET u.senha = ? WHERE u.email = ?", nativeQuery = true)
+    void updatePasswordByEmail(String senha, String email);
 }
