91e1d47d6e7c5f2c9b7f88ea7cac4486cacfc654
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-08-23 11:53:45 -0300

    Criação dos serviços básicos de Cliente e Prestador

--- src/main/java/com/soulcode/goserviceapp/service/PrestadorService.java
+++ src/main/java/com/soulcode/goserviceapp/service/PrestadorService.java
@@ -1,4 +1,35 @@
 package com.soulcode.goserviceapp.service;
 
+import com.soulcode.goserviceapp.domain.Prestador;
+import com.soulcode.goserviceapp.repository.PrestadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PrestadorService {
+
+    @Autowired
+    private PrestadorRepository prestadorRepository;
+
+
+    public Prestador findById(Long id){
+        Optional<Prestador> prestador = prestadorRepository.findById(id);
+        if (prestador.isPresent()){
+            return prestador.get();
+        }
+        else {
+            throw new RuntimeException("Prestador não encontrado");
+        }
+    }
+
+    public Prestador update(Prestador prestador){
+        Prestador updatedPrestador = this.findById(prestador.getId());
+        updatedPrestador.setNome(prestador.getNome());
+        updatedPrestador.setDescricao(prestador.getDescricao());
+        updatedPrestador.setTaxaPorHora(prestador.getTaxaPorHora());
+        return prestadorRepository.save(updatedPrestador);
+    }
 }
