81abd08e3b0888f597fc702cf77ee330ba977eb7
Lucas Takamoto <lucas.takamoto@hotmail.com>
2023-08-24 12:03:34 -0300

    Serviço e Repositorio de cliente e start de agendamento

--- src/main/java/com/soulcode/goserviceapp/repository/ClienteRepository.java
+++ src/main/java/com/soulcode/goserviceapp/repository/ClienteRepository.java
@@ -4,6 +4,9 @@ import com.soulcode.goserviceapp.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByEmail(String email);
 }
--- src/main/java/com/soulcode/goserviceapp/service/ClienteService.java
+++ src/main/java/com/soulcode/goserviceapp/service/ClienteService.java
@@ -3,6 +3,7 @@ package com.soulcode.goserviceapp.service;
 import com.soulcode.goserviceapp.domain.Cliente;
 import com.soulcode.goserviceapp.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,6 +13,21 @@ public class ClienteService {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    public Cliente findAuthenticated(Authentication authentication){
+        if (authentication != null && authentication.isAuthenticated()){
+            Optional<Cliente> cliente = clienteRepository.findByEmail(authentication.getName());
+            if (cliente.isPresent()){
+                return cliente.get();
+            }
+            else{
+                throw new RuntimeException("Cliente não foi encontrado.");
+            }
+        }
+        else{
+            throw new RuntimeException("Não autenticamos o cliente.");
+        }
+    }
 
     public Cliente findById(Long id){
         Optional<Cliente> cliente = clienteRepository.findById(id);
