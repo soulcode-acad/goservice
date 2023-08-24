c627843e5c2224e73b71ad166df72033ed59200f
drygalapedro <drygalapedro@gmail.com>
2023-08-24 13:12:49 -0300

    agendamento

---
+++ src/main/java/com/soulcode/goserviceapp/service/AgendamentoService.java
@@ -0,0 +1,6 @@
+package com.soulcode.goserviceapp.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+
+public class AgendamentoService {
