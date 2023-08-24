aaa12f97e5cc3b97582195e190ae0a47606e5864
victoricoma-soulcode <victor.icoma@soulcode.com>
2023-08-24 12:00:58 -0300

    Seviço e Repositório de cliente e start de agendamento.

--- src/main/java/com/soulcode/goserviceapp/service/ClienteService.java
+++ src/main/java/com/soulcode/goserviceapp/service/ClienteService.java
@@ -3,15 +3,30 @@ package com.soulcode.goserviceapp.service;
 import com.soulcode.goserviceapp.domain.Cliente;
 import com.soulcode.goserviceapp.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.OptionalInt;
 
 @Service
 public class ClienteService {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    public Cliente findAuthenticated(Authentication authentication){
+        if (authentication != null && authentication.isAuthenticated()){
+            Optional<Cliente> cliente = clienteRepository.findByEmail(authentication.getName());
+            if(cliente.isPresent()){
+                return cliente.get();
+            } else {
+                throw new RuntimeException("Cliente não foi encontrado");
+            }
+        } else {
+            throw  new RuntimeException("Não autenticamos o Cliente.");
+        }
+    }
 
     public Cliente findById(Long id){
         Optional<Cliente> cliente = clienteRepository.findById(id);
