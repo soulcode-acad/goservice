2066120c16362ffc07424f2187258b75931cbacc
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-08-24 15:28:48 -0300

    Criação de usuário pela area administrativa.

--- src/main/java/com/soulcode/goserviceapp/controller/AdministradorController.java
+++ src/main/java/com/soulcode/goserviceapp/controller/AdministradorController.java
@@ -1,12 +1,21 @@
 package com.soulcode.goserviceapp.controller;
 
+import com.soulcode.goserviceapp.domain.Usuario;
+import com.soulcode.goserviceapp.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping(value = "/admin")
 public class AdministradorController {
+
+    @Autowired
+    private UsuarioService usuarioService;
 
     @GetMapping(value = "/servicos")
     public String servicos() {
@@ -16,5 +25,16 @@ public class AdministradorController {
     @GetMapping(value = "/usuarios")
     public String usuarios() {
         return "usuariosAdmin";
+    }
+
+    @PostMapping(value = "/usuarios")
+    public String createUser(Usuario usuario, RedirectAttributes attributes) {
+        try {
+            usuarioService.createUser(usuario);
+            attributes.addFlashAttribute("successMessage", "Novo usuário cadastrado.");
+        } catch (Exception ex) {
+            attributes.addFlashAttribute("errorMessage", "Erro ao cadastrar novo usuário.");
+        }
+        return "redirect:/admin/usuarios";
     }
 }
