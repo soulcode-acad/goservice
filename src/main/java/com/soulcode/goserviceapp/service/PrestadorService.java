247574d4054aec5b1e309d31f166741b38201a05
victoricoma-soulcode <victor.icoma@soulcode.com>
2023-08-24 10:46:19 -0300

    Adição dos métodos de relação entre entidades.

--- src/main/java/com/soulcode/goserviceapp/service/PrestadorService.java
+++ src/main/java/com/soulcode/goserviceapp/service/PrestadorService.java
@@ -1,10 +1,13 @@
 package com.soulcode.goserviceapp.service;
 
 import com.soulcode.goserviceapp.domain.Prestador;
+import com.soulcode.goserviceapp.domain.Servico;
 import com.soulcode.goserviceapp.repository.PrestadorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -12,6 +15,21 @@ public class PrestadorService {
     @Autowired
     private PrestadorRepository prestadorRepository;
 
+    @Autowired
+    private ServicoService servicoService;
+
+    public Prestador findAuthenticated(Authentication authentication){
+        if(authentication != null && authentication.isAuthenticated()){
+            Optional<Prestador> prestador = prestadorRepository.findByEmail(authentication.getName());
+            if(prestador.isPresent()){
+                return prestador.get();
+            } else {
+                throw  new RuntimeException("Prestador não Encontrado.");
+            }
+        } else {
+            throw new RuntimeException("Não autenticado");
+        }
+    }
     public Prestador findById(Long id){
         Optional<Prestador> prestador = prestadorRepository.findById(id);
         if(prestador.isPresent()){
@@ -28,5 +46,23 @@ public class PrestadorService {
         updatedPrestador.setDescricao(prestador.getDescricao());
         updatedPrestador.setTaxaPorHora(prestador.getTaxaPorHora());
         return prestadorRepository.save(updatedPrestador);
+    }
+
+    public void addServicoPrestador(Authentication authentication, Long id){
+        Prestador prestador = findAuthenticated(authentication);
+        Servico servico = servicoService.findById(id);
+        prestador.addEspecialidade(servico);
+        prestadorRepository.save(prestador);
+    }
+
+    public void removeServicoPrestador(Authentication authentication, Long id){
+        Prestador prestador = findAuthenticated(authentication);
+        Servico servico = servicoService.findById(id);
+        prestador.removeEspecialidade(servico);
+        prestadorRepository.save(prestador);
+    }
+
+    public List<Prestador> findByServicoId(Long id){
+        return prestadorRepository.findByServicoId(id);
     }
 }
