b6947d28590508de937c6459433d13f6b2ce3e68
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-08-25 17:11:03 -0300

    Inicialização da tabela de serviços.

--- mysql/SCRIPT_INICIAL.sql
+++ mysql/SCRIPT_INICIAL.sql
@@ -16,7 +16,7 @@ drop table servico;
 select * from usuarios;
 
 insert into usuarios (dtype, nome, email, perfil, senha, habilitado)
-value ( 
+value (
 	"Administrador",
 	"Gabriel Braga",
     "gabriel.braga@soulcode.com",
@@ -26,4 +26,23 @@ value (
 ); # 123456
 
 update usuarios set habilitado = 0 where id in (1, 7, 9);
+
+update usuarios set habilitado = 1 where id = 5;
+
+select * from servicos;
+
+INSERT INTO servicos (categoria, descricao, nome) 
+VALUES 
+    ('Limpeza', 'Serviço de limpeza residencial', 'Limpeza Residencial'),
+    ('Treinamento', 'Treinamento corporativo em liderança', 'Treinamento em Liderança'),
+    ('Saúde', 'Consulta médica de clínico geral', 'Consulta Clínico Geral'),
+    ('Desenvolvimento', 'Desenvolvimento de aplicativo móvel', 'Desenvolvimento de App'),
+    ('Design', 'Design de logotipo para empresas', 'Design de Logotipo'),
+    ('Limpeza', 'Limpeza de escritórios comerciais', 'Limpeza Comercial'),
+    ('Treinamento', 'Treinamento em habilidades de comunicação', 'Treinamento em Comunicação'),
+    ('Saúde', 'Sessão de fisioterapia para reabilitação', 'Fisioterapia de Reabilitação'),
+    ('Desenvolvimento', 'Desenvolvimento de site institucional', 'Desenvolvimento de Site Institucional'),
+    ('Design', 'Design de interface de usuário para aplicativos', 'Design de Interface de Aplicativo');
+
+delete from servicos;
 
