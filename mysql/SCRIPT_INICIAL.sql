e8744a45dad44612fe61555811171d0c5589003d
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-08-28 15:47:19 -0300

    Alterar dados do cliente.

--- mysql/SCRIPT_INICIAL.sql
+++ mysql/SCRIPT_INICIAL.sql
@@ -1,36 +1,35 @@
 
-drop database goservice_db;
 
+drop database goservice_db;
 create database goservice_db;
 
 use goservice_db;
 
-show tables;
+describe usuarios;
+describe prestadores_servicos;
 
-drop table agendamentos;
+drop table usuarios;
 
-drop table prestadores_servicos;
+delete from usuarios where id = 7;
 
-drop table servico;
-
+-- CONSULTAS
 select * from usuarios;
-
-insert into usuarios (dtype, nome, email, perfil, senha, habilitado)
-value (
-	"Administrador",
-	"Gabriel Braga",
-    "gabriel.braga@soulcode.com",
-    "ADMIN",
-    "$2a$10$r3r9V682sIhE/61jZjqkauT.08pxrBx.GE1T.yEogN5r8Ly2S8eTK",
-    1
-); # 123456
-
-update usuarios set habilitado = 0 where id in (1, 7, 9);
-
-update usuarios set habilitado = 1 where id = 5;
-
 select * from servicos;
+select * from prestadores_servicos;
 
+delete from prestadores_servicos where servico_id = 20;
+delete from servicos where id = 20;
+
+SELECT s.* FROM servicos s
+JOIN prestadores_servicos ps ON s.id = ps.servico_id
+JOIN usuarios u ON u.id = ps.prestador_id
+WHERE u.email = "leonardo@mail.com";
+
+-- INSERT ADMIN
+insert into usuarios (dtype, nome, email, perfil, senha, habilitado)
+value ("Administrador", "Gabriel Braga", "gabriel.braga@soulcode.com", "ADMIN", "$2a$10$r3r9V682sIhE/61jZjqkauT.08pxrBx.GE1T.yEogN5r8Ly2S8eTK", 1); # 123456
+
+-- POPULATE
 INSERT INTO servicos (categoria, descricao, nome) 
 VALUES 
     ('Limpeza', 'Serviço de limpeza residencial', 'Limpeza Residencial'),
@@ -44,5 +43,35 @@ VALUES
     ('Desenvolvimento', 'Desenvolvimento de site institucional', 'Desenvolvimento de Site Institucional'),
     ('Design', 'Design de interface de usuário para aplicativos', 'Design de Interface de Aplicativo');
 
-delete from servicos;
-
+INSERT INTO prestadores_servicos (servico_id, prestador_id)
+VALUES
+    (11, 11),
+    (13, 12),
+    (15, 11),
+    (16, 12),
+    (17, 11),
+    (18, 12),
+    (20, 11),
+    (21, 12),
+    (22, 11),
+    (11, 12),
+    (13, 11),
+    (15, 12),
+    (16, 11),
+    (17, 12),
+    (18, 11),
+    (20, 12),
+    (21, 11),
+    (22, 12),
+    (11, 11),
+    (13, 12),
+    (15, 11),
+    (16, 12),
+    (17, 11),
+    (18, 12),
+    (20, 11),
+    (21, 12),
+    (22, 11),
+    (11, 12),
+    (13, 11),
+    (15, 12);
