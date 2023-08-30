a878f203e0b8d1757e69d59a8da9e6c2b761e4d3
F-Gabriel-Braga <gabrielbraga0712@gmail.com>
2023-08-30 19:05:09 -0300

    Aplicando conceitos de REST.

---
+++ src/main/java/com/soulcode/goserviceapp/config/WebConfiguration.java
@@ -0,0 +1,24 @@
+package com.soulcode.goserviceapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration  {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
