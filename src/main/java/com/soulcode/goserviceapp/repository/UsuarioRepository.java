e3d2541a8e029799fbbf846d6ab791a12a7c8401
Gustavo <gustavo27hs@gmail.com>
2023-09-01 09:25:22 -0300

    Criação do filtro de nomes por usuario na area de administrador

--- src/main/java/com/soulcode/goserviceapp/repository/UsuarioRepository.java
+++ src/main/java/com/soulcode/goserviceapp/repository/UsuarioRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -20,4 +21,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     @Modifying
     @Query(value = "UPDATE usuarios u SET u.habilitado = ? WHERE u.id = ?", nativeQuery = true)
     void updateEnableById(boolean habilitado, Long id);
+
+    @Query(value = "SELECT * FROM usuarios u WHERE u.nome LIKE ?%", nativeQuery = true)
+    List<Usuario> findUsuarioByNome(String nome);
 }
