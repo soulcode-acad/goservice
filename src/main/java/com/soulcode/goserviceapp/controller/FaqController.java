bf876eb75ffac6f2e5f8066bcab17318831e487e
matheus-fraga <matheuscarlosfraga@gmail.com>
2023-09-01 16:15:52 -0300

    Construção e mapeamento da página de perguntas frequentes

---
+++ src/main/java/com/soulcode/goserviceapp/controller/FaqController.java
@@ -0,0 +1,13 @@
+package com.soulcode.goserviceapp.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/faq")
+public class FaqController {
+    @GetMapping
+    public String faq(){
+        return "perguntasFrequentes";
+    }
+}
