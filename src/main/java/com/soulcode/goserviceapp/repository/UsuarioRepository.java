f341da8391e0fd84ff6985ca61e175ee1f700ca1
LudmilaASouza <ludmilasouz.a@hotmail.com>
2023-09-01 13:34:03 -0300

    "Consulta que busca os números de usuarios por perfil."

--- src/main/java/com/soulcode/goserviceapp/repository/UsuarioRepository.java
+++ src/main/java/com/soulcode/goserviceapp/repository/UsuarioRepository.java
@@ -1,11 +1,13 @@
 package com.soulcode.goserviceapp.repository;
 
 import com.soulcode.goserviceapp.domain.Usuario;
+import com.soulcode.goserviceapp.domain.enums.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -20,4 +22,8 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     @Modifying
     @Query(value = "UPDATE usuarios u SET u.habilitado = ? WHERE u.id = ?", nativeQuery = true)
     void updateEnableById(boolean habilitado, Long id);
+
+    @Query(value= "SELECT perfil, COUNT(perfil) " +
+            " FROM usuarios GROUP BY perfil ", nativeQuery = true)
+    List<Usuario> countUsersByProfile(Perfil perfil);
 }
