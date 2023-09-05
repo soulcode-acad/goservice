2d434a0a40ab130c73a36d6bbbbb99c69db34596
Lucas Figueiredo <91294318+DomLucaz@users.noreply.github.com>
2023-09-05 09:24:02 -0300

    Feat: Implementação da página termos e condições e pólitica de privacidade

--- src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
+++ src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.soulcode.goserviceapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,4 +13,10 @@ public class HomeController {
     public String home(){
         return "home";
     }
+
+    @GetMapping(value = "/politicaTermos")
+    public String politicatermos(){
+        return "politicaTermos";
+    }
+
 }
