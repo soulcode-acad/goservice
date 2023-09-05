a26bc2415ad96efa370e6dcf6c4df7719375e286
FranciscoBolzachini <chicorocha.fb@gmail.com>
2023-09-05 10:09:25 -0300

    termocondicoes

--- src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
+++ src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
@@ -12,4 +12,11 @@ public class HomeController {
     public String home(){
         return "home";
     }
+
+    @GetMapping (value ="/politica")
+    public String politica () {return "politica";}
+
+    @GetMapping (value ="/termoCondicoes")
+    public String termoCondicoes () {return "termoCondicoes";}
+
 }
